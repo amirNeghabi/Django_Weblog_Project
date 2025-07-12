94c2bf897daf8b72c13c1609b807354452dfe500
Amir Neghabi <Amirrezaneghabi001@gmail.com>
2025-07-12 18:50:31 +0330

    update models.py file

--- blog/models.py
+++ blog/models.py
@@ -30,7 +30,7 @@ class Comment(models.Model):
     text = models.TextField()
     datetime_created = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(get_user_model(),on_delete = models.CASCADE)
-    post = models.ForeignKey(Post,on_delete = models.CASCADE)
+    post = models.ForeignKey(Post,on_delete = models.CASCADE,related_name='comments')
 
     def __str__(self):
         return self.text
